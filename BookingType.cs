647d5c8cf489b88f2d60a09537e0764e74d5c426
Noah Yannis Guderjahn <noah.guderjahn@gmx.de>
2025-01-17 15:41:06 +0100

    Anzeigenamen zu Enum-Werten mappen, CSS-Verbesserungen

--- BookingType.cs
+++ BookingType.cs
@@ -1,8 +1,15 @@
-﻿namespace EasyInvoice;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyInvoice;
 
 public enum BookingType
 {
-    HalfBoard, // Halbpension
-    FullBoard, // Vollpension
-    SpecialOffer // Spezialangebot
+    [Display(Name = "Halbpension")]
+    HalfBoard,
+
+    [Display(Name = "Vollpension")]
+    FullBoard,
+
+    [Display(Name = "Spezialangebot")]
+    SpecialOffer
 }
